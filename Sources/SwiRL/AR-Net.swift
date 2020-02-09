6c1a8eee2ddd7fe0cfd2582f2ee6a585c3b321bc
Maxim Volgin <maxim.volgin@me.com>
2020-02-09 21:47:07 +0100

    arnet wip

--- Sources/SwiRL/AR-Net.swift
+++ Sources/SwiRL/AR-Net.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 public struct ARNet {
+    
+    public init() {}
+    
     let L = MSE
     
     static let s = 1.0
@@ -15,22 +18,28 @@ public struct ARNet {
     static let c₁ = 3.0 // for normalized data and coeff within [0,1]
     static let c₂ = 3.0 // for normalized data and coeff within [0,1]
     
-    static let p = 12 // TEMP
-    var θ: [Double] = Array.init(repeating: Double.random(in: 0...1), count: ARNet.p)
+    public static let p = 12 // TEMP
+    public var θ: [Double] = Array.init(repeating: Double.random(in: 0...1), count: ARNet.p)
     
-    var λ: Double { get { ARNet.c * (1.0 - ARNet.s) / ARNet.s } }
+    public var λ: Double { get { ARNet.c * (1.0 - ARNet.s) / ARNet.s } }
     
     /**
      regularization term
      */
-    var R: Double { get { ∑self.θ.map { θ in 2 * σ(ARNet.c₁ * pow(abs(θ), 1.0 / ARNet.c₂)) - 1.0} / Double(ARNet.p) } }
+    public var R: Double { get { ∑self.θ.map { θ in 2 * σ(ARNet.c₁ * pow(abs(θ), 1.0 / ARNet.c₂)) - 1.0} / Double(ARNet.p) } }
     
     /**
      L(y,ŷ,θ) + λ(s) * R(θ)
      */
-    func cost(_ y: [Double], _ ŷ: [Double]) -> Double {
+    public func cost(_ y: [Double], _ ŷ: [Double]) -> Double {
         assert(y.count == ŷ.count && ŷ.count == self.θ.count, "all lengths must be same as θ's")
         return L(y, ŷ) + self.λ * self.R
     }
+    
+    public func predict(_ x: [Double]) -> Double {
+        assert(x.count == self.θ.count, "length must be same as θ's")
+        return ∑zip(x, self.θ).map { (x, θ) in θ * x }
+    }
+    
     
 }
--- SwiRL-Playground/SwiRL-Playground.playground/Pages/Distance.xcplaygroundpage/Contents.swift
+++ SwiRL-Playground/SwiRL-Playground.playground/Pages/Distance.xcplaygroundpage/Contents.swift
@@ -7,4 +7,10 @@ let y = [1.0, 2.0, 3.0]
 let ŷ = [1.5, 2.5, 3.5]
 let mse = MSE(y, ŷ)
 
+let arnet = ARNet()
+let x = Array.init(repeating: Double.random(in: 0...1), count: ARNet.p)
+let p = arnet.predict(x)
+
+
+
 //: [Next](@next)
