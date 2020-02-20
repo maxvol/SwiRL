911ab256cca45ed1a82677cb310dfd801f8229a7
Maxim Volgin <maxim.volgin@me.com>
2020-02-20 18:53:55 +0100

    wip

--- SwiRL-Playground/SwiRL-Playground.playground/Pages/VAE.xcplaygroundpage/Contents.swift
+++ SwiRL-Playground/SwiRL-Playground.playground/Pages/VAE.xcplaygroundpage/Contents.swift
@@ -1,7 +1,12 @@
 //: [Previous](@previous)
 
 import Foundation
+import SwiRL
 
-var str = "Hello, playground"
+var matrix = Matrix<Double>(0.0, shape: 10, 5, 2)
+//print("\(matrix.count)")
+matrix[7, 3, 1] = 6.9
+print("\(matrix[7, 3, 1])")
+
 
 //: [Next](@next)
