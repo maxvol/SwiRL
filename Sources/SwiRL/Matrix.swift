b56510d86d2231fdbbd470dc4c3dcea683387539
Maxim Volgin <maxvol@users.noreply.github.com>
2020-02-20 14:56:01 +0100

    Create Matrix.swift

---
+++ Sources/SwiRL/Matrix.swift
@@ -0,0 +1,39 @@
+public struct Matrix<T: Numeric> {
+  var grid: [T] = []
+  public let shape: [Int]
+  public var count: Int { get { self.shape.reduce(1,*) } }
+
+  public init(_ value: T, shape: Int...) {
+    self.shape = shape
+    self.grid = Array(repeating: value, count: self.count)
+  }
+
+  func indexIsValid(_ index: [Int]) -> Bool {
+    return zip(index, self.shape)
+      .map { (i: Int, count: Int) -> Bool in i < count }
+      .reduce(true) { (s, v) -> Bool in s && v }
+  }
+
+  func index(_ index: [Int]) -> Int {
+    // i * J * K + j * K + k
+    var location = 0
+    for (i, j) in index.enumerated() {
+      let tail = shape[(i + 1)...]
+      let multiplier = tail.reduce(1, *)
+      location += j * multiplier
+    }
+    return location
+  }
+
+  public subscript(_ index: Int...) -> T {
+        get {
+            assert(indexIsValid(index), "Index out of range")
+            return grid[self.index(index)]
+        }
+        set {
+            assert(indexIsValid(index), "Index out of range")
+            grid[self.index(index)] = newValue
+        }
+    }
+
+}
