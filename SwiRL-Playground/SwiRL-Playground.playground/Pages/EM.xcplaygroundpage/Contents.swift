764e60fa0e0156c08d5aaf010776a756a68bee29
Maxim Volgin <maxvol@users.noreply.github.com>
2020-02-20 16:15:11 +0100

    Update Contents.swift

--- SwiRL-Playground/SwiRL-Playground.playground/Pages/EM.xcplaygroundpage/Contents.swift
+++ SwiRL-Playground/SwiRL-Playground.playground/Pages/EM.xcplaygroundpage/Contents.swift
@@ -27,7 +27,10 @@ import SwiRL
 // L <= log p(X)
 
 
-var str = "Hello, playground"
+var matrix = Matrix<Double>(0.0, shape: 10, 5, 2)
+//print("\(matrix.count)")
+matrix[7, 3, 1] = 6.9
+print("\(matrix[7, 3, 1])")
 
 
 
