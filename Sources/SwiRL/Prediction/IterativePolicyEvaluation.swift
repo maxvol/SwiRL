7efec319b4ec8e2f55614945b40fdd7bb0c21aae
Maxim Volgin <maxim.volgin@me.com>
2020-08-08 22:15:56 +0200

    SWF policy eval playground

--- Sources/SwiRL/Prediction/IterativePolicyEvaluation.swift
+++ Sources/SwiRL/Prediction/IterativePolicyEvaluation.swift
@@ -7,25 +7,29 @@
 
 import Foundation
 
+public typealias RLState1 = Int
+public typealias RLValue = Float
 
-typealias RLState1 = Int
-typealias RLValue = Float
+public protocol RLAction1 {}
 
-protocol RLAction1 {
-    associatedtype Item
-}
-
-protocol RLPolicy1 {
+public protocol RLPolicy1 {
     associatedtype A: RLAction1
     
     func action(for state: RLState1) -> A
 }
 
-struct RLOutcome {
-    let probability: RLValue
-    let next: RLState1
-    let reward: RLValue
-    let done: Bool
+public struct RLOutcome {
+    public let probability: RLValue
+    public let next: RLState1
+    public let reward: RLValue
+    public let done: Bool
+    
+    public init(probability: RLValue, next: RLState1, reward: RLValue, done: Bool) {
+        self.probability = probability
+        self.next = next
+        self.reward = reward
+        self.done = done
+    }
 }
 
 //typealias RLActionToOutcome = [RLAction1: [RLOutcome]]
@@ -35,7 +39,8 @@ struct RLOutcome {
 //    let P: RLStateToActionToOutcome = [:]
 //}
 
-protocol RLEnvironment1 {
+public protocol RLEnvironment1 {
+    
     associatedtype A: RLAction1
     
     var stateSpace: [RLState1] { get }
@@ -43,30 +48,58 @@ protocol RLEnvironment1 {
     func outcomeSpace(for state: RLState1, action: A) -> [RLOutcome]
 }
 
-struct IterativePolicyEvaluation<E: RLEnvironment1, P: RLPolicy1> where E.A == P.A {
-    
-    // policy_evaluation(pi, P, gamma=1.0, theta=1e-10)
-    //  for s in range(len(P)):
-    //      for prob, next, reward, done in P[s][pi(s)]:
-    //
-    // P -> [state]
-    // pi(s) -> a
-    // P[s] -> A(s)
-    // P[s][a] -> [prob, next, reward, done] // len(p,n,r,d) = len(A(s))
+fileprivate let zero: RLValue = 0.0
 
+// reference implementation, very inefficient
+public class IterativePolicyEvaluation<E: RLEnvironment1, P: RLPolicy1> where E.A == P.A {
     
-    func evaluate(environment: E, policy: P, gamma: RLValue = 1.0, theta: RLValue = 1e-10) {
-        //
-        for state in environment.stateSpace {
-            //
-            let action = policy.action(for: state)
-            for outcome in environment.outcomeSpace(for: state, action: action) {
+    private (set) var bootstrap: [RLState1: RLValue] = [:]
+    
+    public init() {}
+    
+    public func evaluate(environment: E, policy: P, gamma: RLValue = 1.0, theta: RLValue = 1e-10, maxIterationCount: Int = 1000) {
+               
+        for iteration in 0..<maxIterationCount {
+            
+            var V: [RLState1: RLValue] = [:]
+        
+            for state in environment.stateSpace {
+                var value: RLValue = 0.0
+                let action = policy.action(for: state)
                 
+                for outcome in environment.outcomeSpace(for: state, action: action) {
+                    value += self.value(for: outcome, gamma: gamma)
+                }
+                
+                V[state] = value
             }
             
-        }
+            let didConverge = self.didConverge(V, theta: theta)
+            self.bootstrap = V
+
+            print("#\(iteration): \(V)")
+            
+            if didConverge {
+                print("converged!")
+                break
+            }
         
+        }
     }
     
+    private func value(for outcome: RLOutcome, gamma: RLValue) -> RLValue {
+        let expected = outcome.done ? zero : (gamma * (self.bootstrap[outcome.next] ?? zero))
+        return outcome.probability * (outcome.reward + expected)
+    }
     
+    private func didConverge(_ V: [RLState1: RLValue], theta: RLValue) -> Bool {
+        zip(
+            bootstrap.sorted { $0.key < $1.key },
+            V.sorted { $0.key < $1.key }
+        )
+        .map { abs($0.value - $1.value) < theta }
+        .filter { !$0 }
+        .count > 0
+    }
+        
 }
--- SwiRL.playground/Pages/IterativePolicyEvaluation.xcplaygroundpage/Contents.swift
+++ SwiRL.playground/Pages/IterativePolicyEvaluation.xcplaygroundpage/Contents.swift
@@ -1,16 +1,102 @@
 //: [Previous](@previous)
 
+import Foundation
 import SwiRL
 
-//typealias RLActionToOutcome = [RLAction1: [RLOutcome]]
-//typealias RLStateToActionToOutcome = [RLState1: RLActionToOutcome]
-//
-//struct RLModel {
-//    let P: RLStateToActionToOutcome = [:]
+enum SWFAction: RLAction1 {
+    case left
+    case right
+}
+
+enum SWFOutcome: RLValue {
+    case forward = 0.50
+    case stay = 0.3333
+    case backward = 0.1666
+}
+
+//public struct RLOutcome {
+//    let probability: RLValue
+//    let next: RLState1
+//    let reward: RLValue
+//    let done: Bool
 //}
 
-let ipe = IterativePolicyEvaluation()
+typealias RLActionToOutcome = [SWFAction: [RLOutcome]]
+typealias RLStateToActionToOutcome = [RLState1: RLActionToOutcome]
 
-ipe.evaluate(policy)
+struct SWF: RLEnvironment1 {
+    typealias A = SWFAction
+    
+    private(set) var P: RLStateToActionToOutcome = [
+        0: [
+            .left: [RLOutcome(probability: 1.0, next: 0, reward: 0.0, done: true)],
+            .right: [RLOutcome(probability: 1.0, next: 0, reward: 0.0, done: true)]
+        ],
+        1: [
+            .left: [],
+            .right: []
+        ],
+        2: [
+            .left: [],
+            .right: []
+        ],
+        3: [
+            .left: [],
+            .right: []
+        ],
+        4: [
+            .left: [],
+            .right: []
+        ],
+        5: [
+            .left: [],
+            .right: []
+        ],
+        6: [
+            .left: [RLOutcome(probability: 1.0, next: 0, reward: 0.0, done: true)],
+            .right: [RLOutcome(probability: 1.0, next: 0, reward: 0.0, done: true)]
+        ],
+    ]
+    
+    var stateSpace: [RLState1] { get { Array(P.keys) } }
+    
+    func outcomeSpace(for state: RLState1, action: A) -> [RLOutcome] {
+        P[state]![action]!
+    }
+
+    public init() {
+        for key in P.keys {
+            guard key != 0 && key != 6 else {
+                continue
+            }
+            let lhs = key - 1
+            let rhs = key + 1
+            let reward: RLValue = key == 5 ? 1.0 : 0.0
+            P[key] = [
+                .left: [
+                    RLOutcome(probability: SWFOutcome.forward.rawValue, next: lhs, reward: 0.0, done: true),
+                    RLOutcome(probability: SWFOutcome.stay.rawValue, next: key, reward: 0.0, done: false),
+                    RLOutcome(probability: SWFOutcome.backward.rawValue, next: rhs, reward: reward, done: false)
+                ],
+                .right: [
+                    RLOutcome(probability: SWFOutcome.forward.rawValue, next: rhs, reward: reward, done: true),
+                    RLOutcome(probability: SWFOutcome.stay.rawValue, next: key, reward: 0.0, done: false),
+                    RLOutcome(probability: SWFOutcome.backward.rawValue, next: lhs, reward: 0.0, done: false)
+                ]
+            ]
+        }
+    }
+    
+}
+
+struct SWFPolicy: RLPolicy1 {
+    typealias A = SWFAction
+    func action(for state: RLState1) -> A {
+        .left
+    }
+}
+
+let ipe = IterativePolicyEvaluation<SWF, SWFPolicy>()
+ipe.evaluate(environment: SWF(), policy: SWFPolicy())
 
 //: [Next](@next)
