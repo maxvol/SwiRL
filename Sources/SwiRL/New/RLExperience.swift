5d0534b57d80512d1ec7464a0f59950659529097
Maxim Volgin <maxim@MacBookAir101.fritz.box>
2021-03-23 13:12:28 +0100

    wip

--- Sources/SwiRL/New/RLExperience.swift
+++ Sources/SwiRL/New/RLExperience.swift
@@ -18,5 +18,33 @@ public struct RLExperience<Observation: RLObservation, Action: RLAction, Value: 
     public let nextObservation: Observation
     /** episode is finished */
     public let isFinal: Bool
+    
+    public init(observation: Observation, action: Action, reward: Value, nextObservation: Observation, isFinal: Bool) {
+        self.observation = observation
+        self.action = action
+        self.reward = reward
+        self.nextObservation = nextObservation
+        self.isFinal = isFinal
+    }
+    
+    public init(observation: Observation, action: Action, step: RLStep<Observation, Value>) {
+        self.observation = observation
+        self.action = action
+        switch step {
+        case .restart(let nextObservation):
+            self.reward = Value(0)
+            self.nextObservation = nextObservation
+            self.isFinal = false
+        case .transition(let nextObservation, let reward):
+            self.reward = reward
+            self.nextObservation = nextObservation
+            self.isFinal = false
+        case .termination(let nextObservation, let reward):
+            self.reward = reward
+            self.nextObservation = nextObservation
+            self.isFinal = true
+        }
+    }
+    
 }
 
