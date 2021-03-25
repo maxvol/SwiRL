c3783f25f769714046dd6291d986469f9e0f92c8
Maxim Volgin <>
2021-03-25 07:03:55 +0100

    mdp

---
+++ Sources/SwiRL/Graph/RLMDP.swift
@@ -0,0 +1,34 @@
+//
+//  RLMDP.swift
+//  
+//
+//  Created by Maxim Volgin on 22/03/2021.
+//
+
+import Foundation
+
+
+public protocol RLMDP: Graph where V: RLObservation, E: RLTransition {
+    associatedtype A: RLAction where A.ID == E.Action.ID
+
+    func probability(from: V.ID, via: A.ID, to: V.ID) -> E.Value
+    func reward(from: V.ID, via: A.ID, to: V.ID) -> E.Value
+}
+
+
+/*
+class MDP {
+//    let S: RLState
+//    let A: RLAction
+//    let T: RLTransition
+//    let R: RLReward
+//    let S₀: RLState
+//    let 𝛄: RLCoefficient
+//    let H: Int
+}
+
+class POMDP: MDP {
+//    let O: RLObservation
+//    let E: RLProbability // emission probability of O given S
+}
+*/
--- Sources/SwiRL/Graph/RLTransition.swift
+++ Sources/SwiRL/Graph/RLTransition.swift
@@ -7,23 +7,31 @@
 
 import Foundation
 
-//public protocol RLNode: RLState, Vertex {
-//}
+// S × A × S → [0, 1] // P(s'|s, a)
+// S × A × S → R
 
 public protocol RLTransition: Edge {
+    associatedtype Action: RLAction
     associatedtype Value: RLValue
-        
+
+    var via: Action.ID { get }
     var reward: Value { get }
+    /**
+     P(s'|s, a) → [0, 1]
+     */
+    var probability: Value { get }
+}
+
+public extension RLTransition {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(from)
+        hasher.combine(via)
+        hasher.combine(to)
+    }
 }
 
 public extension RLTransition {
     func render() -> String {
-        "\tV\(from) -> V\(to) [label=\"\(reward)\"];"
+        "\tV\(from) -> V\(to) [label=\"A: \(via), P: \(probability), R: \(reward)\"];"
     }
 }
-
-//public protocol Transition: Edge  {
-//    associatedtype Value: RLValue
-//
-//    var probability: Value { get }
-//}
