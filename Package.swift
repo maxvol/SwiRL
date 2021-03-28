3ea868b14c020e3b11f268b4115482d9d7e373c3
Maxim Volgin <>
2021-03-28 15:21:10 +0200

    platforms

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiRL",
+    platforms: [
+        .macOS(.v11), .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
